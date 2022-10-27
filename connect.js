c3d920c39d9eb3d770c379afed70e242ecb67f5a
Mateusz Manka <90689679+devMaslakers@users.noreply.github.com>
2022-10-27 09:13:49 +0200

    Update connect.js

--- connect.js
+++ connect.js
@@ -1,28 +1,29 @@
 const mysql = require('mysql/promise');
 
 
-const connection =  mysql.createConnection({
+const connection = await mysql.createConnection({
   host     : 'localhost',
   user     : 'maslakers',
   password : 'Chm5Tnb2AVrLbMH',
   database : 'pogoda'
 });
 
-connection.connect(function(err) {
-    if(err) {
-        console.error('error connecting: ' + err.stack);
-        return;
-    }
+// connection.connect(function(err) {
+//     if(err) {
+//         console.error('error connecting: ' + err.stack);
+//         return;
+//     }
 
 
-    console.log('Connection was succesful');
-})
+//     console.log('Connection was succesful');
+// })
 
 
 async function SELECT(values, table) {
     const query = `SELECT * FROM ${table} WHERE id == ${values[0]}`
-  
-    const result = await connection.execute(query)
+    
+    //const [rows, fields]
+    return = await connection.execute(query)
 }
 
 
@@ -35,22 +36,28 @@ async function INSERT(values, table) {
   
     const query = `INSERT INTO ${table}(columns[0], columns[])  VALUES(newValues)`
   
-    const result = await connection.execute(query)
+    return await connection.execute(query)
 }
 
 
-async function UPDATE(values, table) {
-    const result = await connection.execute(query)
+async function UPDATE(values, columns, table, id) {
+    let newValues = ""
+  
+    for(let i = 0; i < values.length; i++) {
+      newValues += columns[i] + " = " + values[id] + ", "
+    }
+  
+    const query = `UPDATE ${table} SET ${newValues} WHERE id = ${id}`
+  
+    return await connection.execute(query)
 }
 
 
 
 function DELETE(values, table) {
-    const query = `DELETE FROM ${table} WHERE id == ${values[0]}`
+    const query = `DELETE FROM ${table} WHERE id = ${values[0]}`
   
-    const result = await connection.execute(query)
-   
-    return result[0].solution;
+    return await connection.execute(query)
 }
 
 
