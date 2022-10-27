824737a1ee7a031bdc96b9eccc43f67c7a4a25ad
Mateusz Manka <90689679+devMaslakers@users.noreply.github.com>
2022-10-27 08:43:02 +0200

    Update connect.js

--- connect.js
+++ connect.js
@@ -1,7 +1,7 @@
-const mysql = require('mysql');
+const mysql = require('mysql/promise');
 
 
-const connection = mysql.createConnection({
+const connection =  mysql.createConnection({
   host     : 'localhost',
   user     : 'maslakers',
   password : 'Chm5Tnb2AVrLbMH',
@@ -19,13 +19,48 @@ connection.connect(function(err) {
 })
 
 
-function Select(query) {
-    connection.query(queryOne, function (error, results, fields) {
-        if (error) throw error;
-        
-        return results[0].solution;
-      });
+async function SELECT(values, table) {
+    const query = `SELECT * FROM ${table} WHERE id == ${values[0]}`
+  
+    const result = await connection.execute(query)
 }
+
+
+async function INSERT(values, table) {
+    let newValues = ""
+  
+    values.forEach(value => {
+      newValues += value + ", "
+    })
+  
+    const query = `INSERT INTO ${table}(columns[0], columns[])  VALUES(newValues)`
+  
+    const result = await connection.execute(query)
+}
+
+
+async function UPDATE(values, table) {
+    const result = await connection.execute(query)
+}
+
+
+
+function DELETE(values, table) {
+    const query = `DELETE FROM ${table} WHERE id == ${values[0]}`
+  
+    const result = await connection.execute(query)
+   
+    return result[0].solution;
+}
+
+
+
+async function server() {
+  
+
+}
+
+server()
 
 //const query = '';
 // Select(query)
